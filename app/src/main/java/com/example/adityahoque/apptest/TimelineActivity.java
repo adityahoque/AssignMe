5a6119411b447744244e38b5a24bdf2de5aa660c
adityahoque <aditya.hoque123@gmail.com>
2017-04-14 09:13:36 -0400

    has a timeline once you log in. fabric timeline. this is not the home timeline.

---
+++ app/src/main/java/com/example/adityahoque/apptest/TimelineActivity.java
@@ -0,0 +1,79 @@
+package com.example.adityahoque.apptest;
+
+import android.app.Activity;
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.Intent;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
+import com.twitter.sdk.android.core.TwitterApiClient;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+import com.twitter.sdk.android.core.TwitterCore;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.models.Tweet;
+import com.twitter.sdk.android.core.services.StatusesService;
+import com.twitter.sdk.android.tweetui.CollectionTimeline;
+import com.twitter.sdk.android.tweetui.FixedTweetTimeline;
+import com.twitter.sdk.android.tweetui.Timeline;
+import com.twitter.sdk.android.tweetui.TimelineResult;
+import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
+import com.twitter.sdk.android.tweetui.TwitterListTimeline;
+import com.twitter.sdk.android.tweetui.UserTimeline;
+
+import java.util.List;
+
+import io.fabric.sdk.android.Fabric;
+import retrofit2.Call;
+
+public class TimelineActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timeline);
+
+        final Context context = this;
+
+        TwitterApiClient twitterApiClient = TwitterCore.getInstance().getApiClient();
+// Can also use Twitter directly: Twitter.getApiClient()
+        StatusesService statusesService = twitterApiClient.getStatusesService();
+        Call<List<Tweet>> call = statusesService.homeTimeline(200,null,null,null,null,null,null);
+        call.enqueue(new Callback<List<Tweet>>() {
+            @Override
+            public void success(Result<List<Tweet>> result)
+            {
+                final FixedTweetTimeline homeTimeline = new FixedTweetTimeline.Builder()
+                        .setTweets(result.data)
+                        .build();
+
+                final TweetTimelineListAdapter adapter = new TweetTimelineListAdapter.Builder(context)
+                        .setTimeline(homeTimeline)
+                        .build();
+                ListView listView = (ListView) findViewById(R.id.timeline);
+                listView.setAdapter(adapter);
+
+            }
+
+            public void failure(TwitterException exception) {
+                //Do something on failure
+            }
+        });
+
+    }
+
+    public void homeButtonClick(View v) {
+            startActivity(new Intent(TimelineActivity.this, TimelineActivity.class));
+        }
+
+
+
+}
