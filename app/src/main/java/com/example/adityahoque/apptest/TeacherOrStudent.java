316a99d9931703a4724486c63cdab903eeb38819
adityahoque <aditya.hoque123@gmail.com>
2017-04-19 08:21:11 -0400

    has correct timeline and buttons at bottom of timeline page

---
+++ app/src/main/java/com/example/adityahoque/apptest/TeacherOrStudent.java
@@ -0,0 +1,13 @@
+package com.example.adityahoque.apptest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TeacherOrStudent extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_teacher_or_student);
+    }
+}
