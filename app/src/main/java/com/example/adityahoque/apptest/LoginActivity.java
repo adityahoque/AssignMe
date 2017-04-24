2821f7e6bd1987b600107dca170236dfaf3ae4ff
Gia <glhill@wpi.edu>
2017-04-24 10:35:26 -0400

    Design

--- app/src/main/java/com/example/adityahoque/apptest/LoginActivity.java
+++ app/src/main/java/com/example/adityahoque/apptest/LoginActivity.java
@@ -1,18 +1,18 @@
 package com.example.adityahoque.apptest;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
-import android.widget.ListView;
 import android.widget.Toast;
 
 import com.twitter.sdk.android.Twitter;
-import com.twitter.sdk.android.core.*;
-import com.twitter.sdk.android.core.identity.*;
+import com.twitter.sdk.android.core.Callback;
+import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterAuthConfig;
-import com.twitter.sdk.android.tweetui.TweetTimelineListAdapter;
-import com.twitter.sdk.android.tweetui.UserTimeline;
+import com.twitter.sdk.android.core.TwitterException;
+import com.twitter.sdk.android.core.TwitterSession;
+import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
 import io.fabric.sdk.android.Fabric;
 
@@ -46,7 +46,7 @@ public class LoginActivity extends AppCompatActivity {
                 // with your app's user model
                 String msg = "@" + session.getUserName() + " logged in! (#" + session.getUserId() + ")";
                 Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
-                startActivity(new Intent(getApplicationContext(), TimelineActivity.class));
+                startActivity(new Intent(getApplicationContext(), TeacherOrStudentActivity.class));
             }
             @Override
             public void failure(TwitterException exception) {
